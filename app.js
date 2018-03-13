da70b7e4c1c399a51d19cdc684aa784a2f258683
Casey Runnells <crunnells@gmail.com>
2018-03-13 13:48:32 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+$(function () {
+    $('#submit').click(function () {
+        var items = 0,
+            message = $('#typetextbox').val();
+
+        $('#messagebox').prepend(addItem(message));
+
+        $('.clear').click(function () {
+            items -= 1;
+            $(this).closest('.li').remove();
+        });
+
+        function addItem(message) {
+            items += 1;
+
+            return "<li class='li'><p class='list-item'><label for='checkbox-" + items + "'><input class='checkbox' type='checkbox' id='checkbox-" + items + "'/><span>" + message + "</span></label><button class='mdl-button mdl-js-button mdl-button--icon clear'><i class='material-icons'>clear</i></button></p></li>";
+        }
+    });
+
+    $('.checkbox').click(function () {
+        $(this).next().toggleClass("crossedOut");
+    });
+
+});
+
+
